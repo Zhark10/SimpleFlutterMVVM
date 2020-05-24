976a863e4235f4aa3174a1f71179558826d014eb
Zhark10 <arkady.zharavin@citronium.biz>
2020-05-24 21:56:33 +0300

    added depends for gmaps (also need to add new app via your google console)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDlHDoupRWSylM8HvsfnOztlKhJ4ddVrqU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
